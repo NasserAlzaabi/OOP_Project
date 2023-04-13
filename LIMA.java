57ad2a585df5f8e8f763701d911b4ce2f2645553
Zayed <zayed.ahmed123321@gmail.com>
2023-04-14 01:21:26 +0400

    Structure

---
+++ LIMA.java
@@ -0,0 +1,30 @@
+public class LIMA {
+    public static void main(String[] args){
+        
+    }
+}
+
+class Technician{
+    private String name,password;
+    private int user_id;
+
+    public Technician(){
+        name = "";
+        user_id = 0;
+        password = "";
+    }
+
+    public Technician(String n, int id, String p){
+        name = n;
+        user_id = id;
+        password = p;
+    }
+}
+
+class Student{
+
+}
+
+class Item{
+
+}
