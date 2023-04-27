de925501ef2e77df33f36b4a385c358c81158b83
AbdulrahmanAlkaabi <662907688aths@gmail.com>
2023-04-27 21:51:13 +0400

    yess

--- Item.java
+++ Item.java
@@ -1,6 +1,61 @@
 package OOP_Project;
 
 public class Item {
+	
+	protected String name, model;
+	protected int value, quantity;
 	boolean Consumable;
 	
+	public Item(String n, String m, int v, int q) {
+		this.name = n;
+		this.model = m;
+		this.value = v;
+		this.quantity = q;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
 }
